bd31fb206ebf1d9c471dcfc6daffd495cf8d50b2
Krisst <krisstopher03@gmail.com>
2022-02-07 12:27:35 -0700

    add

---
+++ Krisst_C#_Projects/Krisst_C#_Sandbox.cs
@@ -0,0 +1,20 @@
+// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+
+namespace Krisst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { SayHI();
+     
+        }    
+        static void SayHI()
+        {
+            Console.WriteLine("Hello Krisst");
+        }
+    }       
+}
---
+++ Krisst_C#_Projects/Krisst_tic_tac_toe.cs
@@ -0,0 +1,41 @@
+// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Krisst
+{
+    class Program
+    {
+        string[] board = CreateBoard();
+        static void DisplayBoard(string[] board)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+        }
+        
+    }       
+}
+
+
+
+
+
+{
+  static void turn_x() 
+  {
+    // code to be executed
+  }
+}
+
+
+{
+  static void turn_o() 
+  {
+    // code to be executed
+  }
+}
+
