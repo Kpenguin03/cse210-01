7eec9c62ec3dc4cb4ce9e9b2092f84ce7e712fcf
Krisst <krisstopher03@gmail.com>
2022-02-07 12:15:42 -0700

    fixed it

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+
+namespace tic_tac_toe_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { SayHI();
+     
+        }    
+        static void SayHI()
+        {
+            Console.WriteLine("Hello Krisst");
+        }
+    }       
+}
